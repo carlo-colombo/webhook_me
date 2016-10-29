887c6ffc09e52ba79c16befb0f94e20da0f3cfc1
Carlo Colombo <carlo.colombo@gmx.com>
2016-10-29 10:38:06 +0100

    fixing indentation

--- lib/webhook_me.ex
+++ lib/webhook_me.ex
@@ -1,24 +1,24 @@
 defmodule WebhookMe do
-    use Maru.Router
-    require Logger
-    alias WebhookMe.Router.TelegramHook
+  use Maru.Router
+  require Logger
+  alias WebhookMe.Router.TelegramHook
 
-    before do
-	plug Plug.Logger
-	plug Plug.Parsers,
-	pass: ["*/*"],
-	json_decoder: Poison,
-	parsers: [:urlencoded, :json, :multipart]
-    end
+  before do
+    plug Plug.Logger
+    plug Plug.Parsers,
+    pass: ["*/*"],
+    json_decoder: Poison,
+    parsers: [:urlencoded, :json, :multipart]
+  end
 
-    mount WebhookMe.Router.Webhook.V1
-    mount WebhookMe.Router.TelegramHook
+  mount WebhookMe.Router.Webhook.V1
+  mount WebhookMe.Router.TelegramHook
 
-    def start(_,[:test]), do: {:ok, self}
+  def start(_,[:test]), do: {:ok, self}
 
-    def start(_,[:dev]) do
-	Logger.info("Setting up polling")
-	TelegramBot.Utils.polling(&TelegramHook.entry_point/1)
-	{:ok, self}
-    end
+  def start(_,[:dev]) do
+    Logger.info("Setting up polling")
+    TelegramBot.Utils.polling(&TelegramHook.entry_point/1)
+    {:ok, self}
+  end
 end
