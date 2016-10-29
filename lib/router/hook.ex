771aac428f01dafca804d1ebeb961c27b272bdd3
Carlo Colombo <carlo.colombo@gmx.com>
2016-10-29 10:29:25 +0100

    cleanup code, moving polling function to Tools module

--- lib/router/hook.ex
+++ lib/router/hook.ex
@@ -14,33 +14,18 @@ defmodule WebhookMe.Router.TelegramHook do
     end
   end
 
+  defp dispatch(chat_id, "/start" <> _) do
+    Nadia.send_message(chat_id, V1.generate_url(chat_id))
+  end
+
+  defp dispatch(chat_id, _) do
+    Nadia.send_message(chat_id, "Only supported command is /start")
+  end
+
+  def entry_point(%{message: %{chat: %{id: chat_id}, text: message}} ) do
+    dispatch(chat_id, message)
+  end
+
   defp get_resp({_, resp}), do: resp
-  defp get_resp(t), do: %{}
-
-  def entry_point(%{"message" => %{"chat" => %{"id" => chat_id}, "text" => text }} ) do
-    Nadia.send_message(chat_id, V1.generate_url(chat_id))
-  end
-  def entry_point(%{message: %{chat: %{id: chat_id}, text: text }} ) do
-    Nadia.send_message(chat_id, V1.generate_url(chat_id))
-  end
-
-
-  def polling(offset \\ 0) do
-    try do
-      {:ok, updates } = Nadia.get_updates([{:offset, offset}])
-      update_id = for update <- updates do
-        entry_point(update)
-        update.update_id
-      end |> List.last
-
-      offset = if update_id == nil do
-        offset
-      else
-        update_id + 1
-      end
-
-      :timer.sleep(2000)
-      spawn(fn  -> polling(offset) end)
-    catch _->nil end
-  end
+  defp get_resp(_), do: %{}
 end
