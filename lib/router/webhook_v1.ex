1c4229b850db528962ff7635f066d053dbd53dca
Carlo Colombo <carlo.colombo@gmx.com>
2016-10-29 11:02:47 +0100

    alternatively use msg or message in the GET

--- lib/router/webhook_v1.ex
+++ lib/router/webhook_v1.ex
@@ -22,6 +22,8 @@ defmodule WebhookMe.Router.Webhook.V1   do
       route_param :hashids do
         params do
           optional :message, type: String
+          optional :msg, type: String
+          exactly_one_of [:message, :msg]
         end
         get do
           json conn, send_message(params)
@@ -31,7 +33,7 @@ defmodule WebhookMe.Router.Webhook.V1   do
 
     def send_message(params) do
       hashids = params[:hashids]
-      message = params[:message] || @default_message
+      message = params[:message] || params[:msg] || @default_message
 
       {:ok, [chat_id]} = Hashids.decode(@coder, hashids)
       {:ok, res} = Nadia.send_message(chat_id, message)
